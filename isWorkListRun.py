0c0fb93efb5ae2da667d3b63b9e0ae6e8880d20d
jiajianchao <jiajianchao108@sina.com>
2015-03-23 18:38:07 -0700

    Judge _pid's existance

--- isWorkListRun.py
+++ isWorkListRun.py
@@ -4,6 +4,10 @@ import os
 import sys
 
 def WorklistRun():
+    if os.path.exist("processPassWorklist_pid"):
+        os.system("rm processPassWorklist_pid")
+    if os.path.exist("processUnprocessWorklist_pid"):
+        os.system("rm processUnprocessWorklist_pid")
     os.system('ps -ef | grep processPassWorklist.py > processPassWorklist_pid')
     os.system('ps -ef | grep processUnprocessWorklist.py > processUnprocessWorklist_pid')
     lines=[line for line in file('processPassWorklist_pid')]
